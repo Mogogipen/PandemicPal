3d4dcb6f9d6abdcdc6bb3f2bb4521b7573217e37
mha943 <matthewhall2943@gmail.com>
2021-04-07 13:30:55 -0600

    Basic Setup of App
    Added a start screen so that we could do a check to see if user has a pet before loading the second page. On you pet page I added 6 buttons, the background image, and a place for your pets name. Gave all of these constrains

--- app/src/main/java/com/example/pandemicpal/MainActivity.kt
+++ app/src/main/java/com/example/pandemicpal/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.pandemicpal
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var startButton = findViewById<Button>(R.id.startButton)
+        // have some kind of code here to check if a pet is present
+        var hasPet = true
+
+        startButton.setOnClickListener {
+
+            if (hasPet){
+                startActivity(Intent(this,PetPage::class.java))
+            }else{
+                // startActivity(Intent(this,SetupNewPet::class.java))
+            }
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/pandemicpal/PetPage.kt
@@ -0,0 +1,11 @@
+package com.example.pandemicpal
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_page)
+    }
+}
