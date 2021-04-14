28b1ba16f2ab0db32b804c2a56c210ae2a8f03b4
mha943 <matthewhall2943@gmail.com>
2021-04-13 19:29:31 -0600

    added some delay to images
    only for the ones that have different images

--- app/src/main/java/com/example/pandemicpal/PetPageActivity.kt
+++ app/src/main/java/com/example/pandemicpal/PetPageActivity.kt
@@ -3,6 +3,8 @@ package com.example.pandemicpal
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.*
 
 
@@ -26,9 +28,19 @@ class PetPageActivity : AppCompatActivity() {
         }
         feedButton.setOnClickListener{
             backgroundImage.setImageResource(R.drawable.room_kitchen)
+            feedButton.isEnabled = false
+            Handler(Looper.getMainLooper()).postDelayed({
+               backgroundImage.setImageResource(R.drawable.room_default)
+                feedButton.isEnabled = true
+            }, 2000)
         }
         walkButton.setOnClickListener{
             backgroundImage.setImageResource(R.drawable.room_meadow)
+            walkButton.isEnabled = false
+            Handler(Looper.getMainLooper()).postDelayed({
+                backgroundImage.setImageResource(R.drawable.room_default)
+                walkButton.isEnabled = true
+            }, 2000)
         }
         playButton.setOnClickListener{
             backgroundImage.setImageResource(R.drawable.room_default)
@@ -41,6 +53,11 @@ class PetPageActivity : AppCompatActivity() {
         }
         toiletButton.setOnClickListener{
             backgroundImage.setImageResource(R.drawable.room_bath)
+            toiletButton.isEnabled = false
+            Handler(Looper.getMainLooper()).postDelayed({
+                backgroundImage.setImageResource(R.drawable.room_default)
+                toiletButton.isEnabled = true
+            }, 2000)
         }
 
     }
