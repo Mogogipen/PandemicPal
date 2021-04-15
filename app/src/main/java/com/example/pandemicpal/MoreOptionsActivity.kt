a416f49b19fd3099c748275ccdb02d3e11df3ce9
mha943 <matthewhall2943@gmail.com>
2021-04-15 13:04:52 -0600

    added alert to delete pet

--- app/src/main/java/com/example/pandemicpal/MoreOptionsActivity.kt
+++ app/src/main/java/com/example/pandemicpal/MoreOptionsActivity.kt
@@ -1,11 +1,13 @@
 package com.example.pandemicpal
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MoreOptionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,9 +19,17 @@ class MoreOptionsActivity : AppCompatActivity() {
         removePetButton.setOnClickListener {
 
             val sharedPreferences : SharedPreferences = getSharedPreferences("sharedPreferences", MODE_PRIVATE)
-            sharedPreferences.edit().remove("hasPet").commit()
-            Toast.makeText(this, "Pet Removed", Toast.LENGTH_SHORT).show()
-            startActivity(Intent(this, MainActivity::class.java))
+            var alert = AlertDialog.Builder(this)
+            alert.setTitle("Remove Pet")
+            alert.setMessage("Are you sure you want to remove your pet?")
+            alert.setPositiveButton("Yes") { dialogInterface: DialogInterface, i: Int ->
+                sharedPreferences.edit().remove("hasPet").commit()
+                Toast.makeText(this, "Pet Removed", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+            }
+            alert.setNegativeButton("No"){ dialogInterface: DialogInterface, i: Int ->}
+            alert.show()
+
         }
     }
 }
